d6cc10d3a241af8504a3f34036ce286556d23e25
Harry Sadler <harrysadlermusic@gmail.com>
2015-08-06 11:57:39 -0700

    correct styles link in index, fix app.js conflict from before, add profile.js

---
+++ client/javascript/profile.js
@@ -0,0 +1,9 @@
+angular.module('TriviaWithFriends.profile', [])
+
+.controller('ProfileController', ['$scope', '$http', function ($scope, $http) {
+
+  $scope.test = 'profile test info';
+
+
+
+}]);
