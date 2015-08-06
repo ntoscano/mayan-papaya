f6559cc926dd23849771c597fec1f67231c17014
David Beckley <beckl.ds@gmail.com>
2015-08-05 19:52:23 -0700

    Serve static files

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 //the node server
 
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, '../client')));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
