dd789d5840754c54e899c5adbf00dc89ee75657a
David Beckley <beckl.ds@gmail.com>
2015-08-06 19:41:13 -0700

    Fix test

--- test/client/TriviaFactorySpec.js
+++ test/client/TriviaFactorySpec.js
@@ -1,19 +1,29 @@
 var expect = chai.expect;
 
 describe('UserController', function() {
-  var Questions;
+  var Questions, $httpBackend;
 
   beforeEach(module('Trivia'));
 
   beforeEach(inject(function($injector) {
     Questions = $injector.get('Questions');
+    $httpBackend = $injector.get('$httpBackend');
   }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
   it('should have a getQuestions method in Questions', function() {
     expect(Questions.getQuestions).to.be.a('function');
   });
 
-  it('should return an array from getQuestions', function() {
-    expect(Questions.getQuestions).to.be.a('function');
+  it('should set questions from HTTP response', function() {
+    var mockQuestions = [{}, {}, {}];
+    $httpBackend.expectGET('/questions').respond(mockQuestions);
+    Questions.getQuestions();
+    $httpBackend.flush();
+    expect(Questions.questions).to.eql(mockQuestions);
   });
 });
