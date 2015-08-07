b2c80ab1474f160bc8dafada078fcf26262f4cf6
Harry Sadler <harrysadlermusic@gmail.com>
2015-08-07 12:26:15 -0700

    add home view and home controller js, add dummy data, add initial tests for HomeController, update app.js routing and index.html

--- client/app.js
+++ client/app.js
@@ -1,20 +1,24 @@
 (function() {
 
-  var app = angular.module('TriviaWithFriends', ['ui.router', 'Trivia', 'Profile']); // need User dependency
+  var app = angular.module('TriviaWithFriends', ['ui.router', 'Trivia', 'Profile', 'Home']); // need User dependency
 
   app.config ([
   '$stateProvider',
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/home',
+      .state('trivia', {
+        url: '/triva',
         templateUrl: 'views/trivia.html'
       })
       .state('profile', {
         url: '/profile',
         templateUrl: 'views/profile.html'
-      });
+      })
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/home.html'
+      });;
     $urlRouterProvider.otherwise('home');
   }]);
 
