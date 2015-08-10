15beef7efcc69324eed277ed2890e20f610d3ddd
David Beckley <beckl.ds@gmail.com>
2015-08-10 10:13:57 -0700

    Add karma to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,17 @@ module.exports = function(grunt) {
   grunt.initConfig({
     mochaTest: {
       src: ['test/server/**/*.js']
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('test', ['mochaTest', 'karma']);
   grunt.registerTask('default', ['test']);
 };
