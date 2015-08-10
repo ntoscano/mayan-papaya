166abd00b689cd3ba97baa1f319d8f00c5ed1057
Eric Fischer <ericf712@gmail.com>
2015-08-10 12:51:33 -0700

    Adds footer controller and script tag

---
+++ client/javascript/footer.js
@@ -0,0 +1,11 @@
+(function() {
+
+  var app = angular.module('Footer',[]);
+
+  app.controller('FooterController', ['$scope', '$location', function($scope, $location) {
+    // Something here
+
+  }]);
+
+
+})();
