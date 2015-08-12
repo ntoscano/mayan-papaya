9fb102e0562f084148f9cbd43f94ac33aad3c631
Harry Sadler <harrysadlermusic@gmail.com>
2015-08-12 09:40:28 -0700

    add ngRepeat for questions, add multiple test questions for testing question navigation

--- client/javascript/trivia.js
+++ client/javascript/trivia.js
@@ -22,10 +22,13 @@
 
     obj.getClue = function(answer) {
       var result = [];
+      var to_ = /([a-zA-Z0-9])/g;
       _.each(answer, function(char) {
-
+        if(char.match(to_) !== null) {
+          result.push('_');
+        } else result.push(char);
       });
-      return result;
+      return result.join('');
     };
 
     obj.getQuestions = function() { // retrieves questions from backend
@@ -35,12 +38,11 @@
         // route returns a list of questions
         obj.questions = data;
 
-        //clean the italics from the answers
+        //clean the italics from the answers and add the clue to the object
         _.each(obj.questions, function(q) {
           q.answer = obj.getCleanAnswer(q.answer);
+          q.clue = obj.getClue(q.answer);
         });
-
-
 
       });
     };
@@ -51,35 +53,80 @@
   app.controller('TriviaController', ['$scope', '$http', 'Questions', function($scope, $http, Questions) {
 
     //sample trivia api response for chai test
-    $scope.questions = [{
-      "id": 152933,
-      "answer": "Syria",
-      "question": "This country is bordered by Turkey to the north & Iraq to the east",
-      "value": 200,
-      "airdate": "2015-01-02T12:00:00.000Z",
-      "created_at": "2015-01-22T02:40:00.134Z",
-      "updated_at": "2015-01-22T02:40:00.134Z",
-      "category_id": 17840,
-      "game_id": 4773,
-      "invalid_count": null,
-      "category": {
-        "id": 17840,
-        "title": "places that end in \"ia\"",
-        "created_at": "2015-01-18T18:16:16.428Z",
-        "updated_at": "2015-01-18T18:16:16.428Z",
-        "clues_count": 10
+    $scope.questions = [
+      {
+        "id": 46207,
+        "answer": "England",
+        "question": "This country's 1689 Bill of Rights stated that no Roman Catholic would ever rule it",
+        "value": 100,
+        "airdate": "2000-11-23T12:00:00.000Z",
+        "created_at": "2014-02-11T23:13:46.149Z",
+        "updated_at": "2014-02-11T23:13:46.149Z",
+        "category_id": 5724,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+          "id": 5724,
+          "title": "catholicism",
+          "created_at": "2014-02-11T23:13:46.044Z",
+          "updated_at": "2014-02-11T23:13:46.044Z",
+          "clues_count": 10
+        }
+      },
+      {
+        "id": 46208,
+        "answer": "The fittest",
+        "question": "Herbert Spencer summed up Darwin's theory as \"Survival of\" these",
+        "value": 100,
+        "airdate": "2000-11-23T12:00:00.000Z",
+        "created_at": "2014-02-11T23:13:46.199Z",
+        "updated_at": "2014-02-11T23:13:46.199Z",
+        "category_id": 5725,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+          "id": 5725,
+          "title": "the mostest",
+          "created_at": "2014-02-11T23:13:46.056Z",
+          "updated_at": "2014-02-11T23:13:46.056Z",
+          "clues_count": 5
+        }
+      },
+      {
+        "id": 46209,
+        "answer": "\"Peter Pan\"",
+        "question": "John Darling, Wendy Darling,Tinker Bell",
+        "value": 100,
+        "airdate": "2000-11-23T12:00:00.000Z",
+        "created_at": "2014-02-11T23:13:46.221Z",
+        "updated_at": "2014-02-11T23:13:46.221Z",
+        "category_id": 5726,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+          "id": 5726,
+          "title": "plays by characters",
+          "created_at": "2014-02-11T23:13:46.068Z",
+          "updated_at": "2014-02-11T23:13:46.068Z",
+          "clues_count": 5
+        }
       }
-    }];
+    ];
 
     $scope.getQuestions = function() {
-      Questions.getQuestions()
-        .success(function(data) {
-          $scope.questions = data;
-          console.log($scope.questions);
-        });
-    };
+      //enable when ready
+      // Questions.getQuestions()
+      //   .success(function(data) {
+      //     $scope.questions = data;
+      //     console.log($scope.questions);
+      //   });
 
-    //disable for testing
+      //for testing only
+      _.each($scope.questions, function(q) {
+        q.answer = Questions.getCleanAnswer(q.answer);
+        q.clue = Questions.getClue(q.answer);
+      });
+    };
     $scope.getQuestions();
 
 
