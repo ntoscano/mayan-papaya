b7455f776649eb75393ea6cef5c8b7cdfef6576e
Harry Sadler <harrysadlermusic@gmail.com>
2015-08-14 09:39:39 -0700

    cleanup signup css, html refactor of signin and signup, add hack for hiding nav on signin and signup

--- client/javascript/navigation.js
+++ client/javascript/navigation.js
@@ -4,11 +4,21 @@
 
   app.controller('NavigationController', ['$scope', '$location', '$state', function($scope, $location, $state) {
 
+    //for changing active nav tab
     $scope.getClass = function(stateName) {
       if (stateName === $state.current.name) {
         return 'active';
       } else {
         return '';
+      }
+    };
+
+    //hack for hiding nav on signin or signup
+    $scope.show = function() {
+      if($location.$$url === '/signin' || $location.$$url === '/signup') {
+        return false;
+      } else {
+        return true;
       }
     };
 
