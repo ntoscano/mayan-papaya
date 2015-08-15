893f3b7de7785f336ea8f9fab538d50260d60772
Harry Sadler <harrysadlermusic@gmail.com>
2015-08-14 20:22:59 -0700

    add sample user data to ProfileController

--- client/javascript/profile.js
+++ client/javascript/profile.js
@@ -28,6 +28,30 @@
 
   app.controller('ProfileController', ['$scope', 'ProfileFactory', 'UserFactory', 'CurrentUser', function($scope, ProfileFactory, UserFactory, CurrentUser) {
 
+    //sample user data from DB
+    $scope.user = {
+      "_id": "55ce9311dda321437709f35c",
+      "salt": "$2a$10$gLPRaKFp3JG6J2M\/VOQ.uu",
+      "username": "Bob",
+      "password": "$2a$10$gLPRaKFp3JG6J2M\/VOQ.uuE67zxeljbSnSp8DfWpwkOMsPHDw1wwW",
+      "__v": 0,
+      "mostRecentGame": {
+        "questionsAnsweredCorrect": 0,
+        "questionsAnswered": 0,
+        "gameTime": 0,
+        "xpEarned": 0,
+        "gameScore": 0
+      },
+      "questionsAnsweredCorrect": 0,
+      "questionsAnswered": 0,
+      "wonLastGame": false,
+      "bestCorrectStreak": 0,
+      "bestGameScore": 0,
+      "gamesPlayed": 0,
+      "totalXp": 0,
+      "userLevel": 1
+    }
+
     ProfileFactory.getUserData(ProfileFactory.getUsername())
       .then(function(data) {
         console.log("profile factory invoked, user data: ", data);
